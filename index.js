d1f5eacb2603716548006368667d62d1cbf0493e
kevin@kevin <kevin1kipkurui@gmail.com>
2022-06-19 12:45:08 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+// Your code goes here
+document.addEventListener( "DOMContentLoaded", addNew) 
+function addNew ()
+{
+    document.getElementById("text").innerHTML = 'This is really cool!';
+}
